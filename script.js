7abbebbfe85c7627c25123d7631cde4cbfda7df5
kdugaspar <kadugaspardesousa@gmail.com>
2024-02-15 18:04:35 -0300

    Atualização

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ let paragrafo = window.document.querySelector("p")
 botao.addEventListener("click",somar)
 
 
-function somar(){
+function somar(){   
 let num1 = prompt("Digite um número: ")
 let num2 = prompt("Digite outro número: ")
 let soma = Number(num1) + Number(num2)
 paragrafo.innerHTML =`A soma entre ${num1} e ${num2} é igual a ${soma}!`
-
-let botao = window.document.createElement("input")
 }
